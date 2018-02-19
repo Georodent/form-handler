1810a31a5bd36f39fddccf2c2c73b7c4c9e66cd1
George R <rodeng@uw.edu>
2018-02-18 21:23:45 -0800

    New files

---
+++ form-handlers.js
@@ -0,0 +1,11 @@
+function capture(){
+  event.preventDefault()
+  console.log("form submitted")
+  
+  var emailAdd =document.custInfo.email.value
+  console.log("Thanks for signing up to our mailing list " + emailAdd)
+  var firstName =document.custInfo.name.value
+  console.log("That's a swell name " + firstName)
+  var lastName =document.custInfo.last.value
+  console.log("Altogether, that sounds great " + firstName + lastName)
+}
